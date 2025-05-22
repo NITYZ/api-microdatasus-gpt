1494c5d19442daaa286d5e4c3735b385e44762c0
NITYZ <luisgustavomf27@gmail.com>
2025-05-22 10:42:52 -0300

    Create  install.R

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages("plumber")
+install.packages("remotes")
+install.packages("read.dbc", repos = "https://packagemanager.posit.co/cran/2024-07-05")
+remotes::install_github("rfsaldanha/microdatasus")
